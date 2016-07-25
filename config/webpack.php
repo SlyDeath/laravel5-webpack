cd789e7a282b32d0b953cbc8e4c7dce6d8a3e6b9
SlyDeath <belyaev.rs@live.ru>
2016-07-25 23:06:12 +0300

    Create webpack.php

---
+++ config/webpack.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    // Build path in public directory
+    'build_path' => 'build',
+
+    // Webpack dev server options
+    'dev_server' => [
+
+        // Https is on?
+        'https' => false,
+
+        // Webpack dev server IP
+        'ip' => '127.0.0.1',
+
+        // Webpack dev server port
+        'port' => 8080,
+    ]
+];
